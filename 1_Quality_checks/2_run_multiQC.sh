4733986612787cd45f6ea256c28221641f5f38f0
AnMaLo <55525638+AnMaLo@users.noreply.github.com>
2022-01-19 08:25:50 +0100

    Update 2_run_multiQC.sh

--- 1_Quality_checks/2_run_multiQC.sh
+++ 1_Quality_checks/2_run_multiQC.sh
@@ -6,9 +6,7 @@
 #SBATCH --time=05:00:00
 #SBATCH --mem=1G
 
-#load modules
-#source /data/courses/rnaseq/breastcancer_de/ananda_workspace/scripts/fastqc_modul.sh
-module use /software/module/
+#load module
 module add UHTS/Analysis/MultiQC/1.8
 
 # go to the QC directory
